15e8186ba86d4a6d21e01d110f509ce2d5414984
sco_tt <scott@scottpinkelman.com>
2014-12-07 15:35:50 -0500

    adding semi-transparent KMZ - take 2

--- assets/cz.js
+++ assets/cz.js
@@ -308,7 +308,7 @@ http://krasimirtsonev.com/blog/article/GoogleMaps-JS-API-address-to-coordinates-
 
 	//start Layers
 
-	var layer = new google.maps.KmlLayer('https://sco-tt.github.io/What-s-My-Climate-Zone/Koeppen-Geiger-GE.kmz',
+	var layer = new google.maps.KmlLayer('https://sco-tt.github.io/What-s-My-Climate-Zone/Koppen-Geiger_SemiT.kmz',
 	{preserveViewport: true});
 
 	//toggle layers
